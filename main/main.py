bdfab498d9a6e9987f99e49525264365525b961e
JussGabz <gabrieli94@hotmail.co.uk>
2023-12-05 10:41:56 +0000

    set up routes for main app

--- main/main.py
+++ main/main.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, abort, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+import requests
 from sqlalchemy import UniqueConstraint
+
+from producer import publish
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:root@db/admin'
@@ -22,9 +25,37 @@ class ProductUser(db.Model):
     UniqueConstraint('user_id', 'product_id', name='user_product_unique')
 
 
-@app.route('/')
+@app.route('/api/products')
 def index():
-    return "Hello"
+    return jsonify(Product.query.all())
+
+@app.route('/api/products/<int:id>/like', methods=['POST'])
+def post_like(id):
+
+    # Make request to receive random user
+    req = requests.get('http://docker.for.mac.localhost:8000/api/user')
+
+    # Return User in json response
+    json = req.json()
+
+    try:
+        # Create product User with User from endpoint
+        product_user = ProductUser(user_id=json['id'], product_id=id)
+        db.session.add(product_user)
+        db.session.commit()
+
+        publish('product_liked', id)
+
+    except:
+        abort(400, 'You already liked this product')
+
+
+    return jsonify({
+        'message': 'success'
+    })
+
+    
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
