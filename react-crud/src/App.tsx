2fde0afdf9e7e28627fbf9a54d1204c2c3a81860
JussGabz <gabrieli94@hotmail.co.uk>
2023-12-24 17:44:33 +0000

    refactor app and add routes

--- react-crud/src/App.tsx
+++ react-crud/src/App.tsx
@@ -1,34 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Nav from './components/Nav';
-import Menu from './components/Menu';
 import Products from './admin/Products';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './main/Main';
+import ProductsCreate from './admin/ProductsCreate';
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-
-<div className="container-fluid">
-  <div className="row">
-    <Menu />
-    <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-    
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' Component={Main}/>
-          <Route path='/admin/Products' Component={Products}/>
-        </Routes>
-      </BrowserRouter>
-
-      
-    </main>
-  </div>
-</div>
-</div>
+    <div className="App">          
+            <BrowserRouter>
+              <Routes>
+                <Route path='/' Component={Main}/>
+                <Route path='/admin/products' Component={Products}/>
+                <Route path='/admin/products/create' Component={ProductsCreate}/>
+              </Routes>
+            </BrowserRouter>
+    </div>
 );
 }
 
