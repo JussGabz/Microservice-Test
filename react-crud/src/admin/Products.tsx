ae98de45811e5bea15b25a3691d2aba8e69b90c8
JussGabz <gabrieli94@hotmail.co.uk>
2023-12-24 17:45:57 +0000

    call back end from product page

--- react-crud/src/admin/Products.tsx
+++ react-crud/src/admin/Products.tsx
@@ -1,31 +1,79 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Wrapper from './Wrapper';
+import { Product } from './interfaces/product';
+import { Link } from 'react-router-dom';
 
 const Products = () => {
-    return (
 
-        <div className="table-responsive small">
-        <table className="table table-striped table-sm">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Header</th>
-              <th scope="col">Header</th>
-              <th scope="col">Header</th>
-              <th scope="col">Header</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1,001</td>
-              <td>random</td>
-              <td>data</td>
-              <td>placeholder</td>
-              <td>text</td>
-            </tr>
-          </tbody>
-        </table>
-        </div>
+    const [products, setProducts] = useState([]);
+    useEffect( () => {
+        (
+            async () => {
+                // Call Back End Product API
+                const response = await fetch('http://localhost:8000/api/products/');
+                
+                // Get Response from Product API
+                const data = await response.json();
+
+                setProducts(data);
+            }
+        )();
+    }, []);
+
+    const del = async (id: number) => {
+        // Add Confirmation Box before product deletion
+        if (window.confirm('Are you sure you want to delete this product?')) {
+            await fetch(`http://127.0.0.1:8000/api/products/${id}`, {
+                method: 'DELETE'
+            });
+
+            setProducts(products.filter(
+                (p: Product) => p.id !== id
+            ));
+        }
+    }
+    
+    return (
+        <Wrapper>
+            <div className='pt-3 pb-2 mb-3 border-bottom'>
+                <div className='btn-toolbar mb-2 mb-md-0'>
+                    <Link to='/admin/products/create' className='btn btn-sm btn-outline-secondary'>Add</Link>
+                </div>
+            </div>
+            <div className="table-responsive small">
+            <table className="table table-striped table-sm">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Image</th>
+                <th scope="col">Title</th>
+                <th scope="col">Likes</th>
+                <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {products.map(
+                    (p: Product) => {
+                    return (
+                        <tr key={p.id}>
+                            <td>{p.id}</td>
+                            <td><img src={p.image} height={"180"}></img></td>
+                            <td>{p.title}</td>
+                            <td>{p.likes}</td>
+                            <td>
+                                <div className='btn-group mr-2'>
+                                    <a href='#' className='btn btn-sm btn-outline-secondary' onClick={() => del(p.id)}> Delete</a>
+                                </div>
+                            </td>
+                        </tr>
+                    )
+                })}
+
+            </tbody>
+            </table>
+            </div>
+        </Wrapper>
     );
 };
 
-export default Products
+export default Products;
