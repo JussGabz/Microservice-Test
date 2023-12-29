267cca75692f546ac64c94fd045f62eecdd50f22
JussGabz <gabrieli94@hotmail.co.uk>
2023-12-29 19:20:29 +0000

    fetch product from admin backend

--- react-crud/src/App.tsx
+++ react-crud/src/App.tsx
@@ -5,6 +5,7 @@ import Products from './admin/Products';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './main/Main';
 import ProductsCreate from './admin/ProductsCreate';
+import ProductsEdit from './admin/ProductsEdit';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
                 <Route path='/' Component={Main}/>
                 <Route path='/admin/products' Component={Products}/>
                 <Route path='/admin/products/create' Component={ProductsCreate}/>
+                <Route path='/admin/products/:id/edit' Component={ProductsEdit}/>
               </Routes>
             </BrowserRouter>
     </div>
