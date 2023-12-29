b73c2f02555d80c0212748eb7b8419f9064c8098
JussGabz <gabrieli94@hotmail.co.uk>
2023-12-29 19:20:54 +0000

    create product component

--- react-crud/src/admin/ProductsCreate.tsx
+++ react-crud/src/admin/ProductsCreate.tsx
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { SyntheticEvent, useState } from "react";
 import Wrapper from "./Wrapper";
+import { Navigate  } from "react-router-dom";
 
 const ProductsCreate = () => {
+    // Create Variables for the state
+    const [title, setTitle] = useState('')
+    const [image, setImage] = useState('')
+    const [redirect, setRedirect] = useState(false)
+
+    const submit = async (e: SyntheticEvent) => {
+        e.preventDefault()
+    
+        await fetch('http://localhost:8000/api/products/', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                title,
+                image
+            })
+        });
+
+        setRedirect(true)
+    }
+
+    if (redirect) {
+        return <Navigate to="/admin/products"/>
+    }
     return (
         <Wrapper>
-            <form>
+            <form onSubmit={submit}>
                 <div className="form-group">
                     <label>Title</label>
-                    <input type="text" className="form-control" name="title"/>
+                    <input type="text" className="form-control" name="title"
+                        onChange={e => setTitle(e.target.value)}/>
                 </div>
                 <div className="form-group">
                     <label>Image</label>
-                    <input type="text" className="form-control" name="image"/>
+                    <input type="text" className="form-control" name="image"
+                        onChange={e => setImage(e.target.value)}/>
                 </div>
                 <button className="btn btn-outline-secondary">Save</button>
             </form>
